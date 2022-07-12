2d2a48abb614e94a1c0f599b94dfcdc73aef67af
Jurien Hamaker <jurien@employes.nl>
2022-07-12 17:39:17 +0200

    fix(Atoms/Loader): fix loader z-index & full width version

--- packages/core/src/components/atoms/loader/loader.component.tsx
+++ packages/core/src/components/atoms/loader/loader.component.tsx
@@ -2,82 +2,94 @@ import { Component, h, Host, Prop } from '@stencil/core';
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
-  tag: 'p-loader',
-  styleUrl: 'loader.component.scss',
-  shadow: true,
+    tag: 'p-loader',
+    styleUrl: 'loader.component.scss',
+    shadow: true,
 })
 export class Loader {
-  /**
-   * Wether to show or hide the loader
-   */
-  @Prop() show: boolean | Observable<boolean> = true;
+    /**
+     * Wether to show or hide the loader
+     */
+    @Prop() show: boolean | Observable<boolean> = true;
 
-  /**
-   * Variant of loader
-   */
-  @Prop() variant: 'inline' | 'full-width' | 'full-screen' | 'modal' = 'inline';
+    /**
+     * Variant of loader
+     */
+    @Prop() variant: 'inline' | 'full-width' | 'full-screen' | 'modal' =
+        'inline';
 
-  /**
-   * Color of the loader
-   */
-  @Prop() color: 'indigo' | 'white' | 'storm' = 'indigo';
+    /**
+     * Color of the loader
+     */
+    @Prop() color: 'indigo' | 'white' | 'storm' = 'indigo';
 
-  /**
-   * !NOT IMPLEMENTED! Modal title for modal variant
-   */
-  @Prop() modalTitle: string;
+    /**
+     * !NOT IMPLEMENTED! Modal title for modal variant
+     */
+    @Prop() modalTitle: string;
 
-  /**
-   * !NOT IMPLEMENTED! Modal description for modal variant
-   */
-  @Prop() modalDescription: string;
+    /**
+     * !NOT IMPLEMENTED! Modal description for modal variant
+     */
+    @Prop() modalDescription: string;
 
-  private _show = this.show;
-  private _showSubscriber: Subscription;
+    private _show = this.show;
+    private _showSubscriber: Subscription;
 
-  private _loader = (<div class={`loader color-${this.color}`}></div>);
+    private _loader = (<div class={`loader color-${this.color}`}></div>);
 
-  componentWillRender() {
-    this._checkShow();
-  }
-
-  componentShouldUpdate() {
-    this._checkShow();
-  }
-
-  render() {
-    if (!this._show) {
-      return;
+    componentWillRender() {
+        this._checkShow();
     }
 
-    if (this.variant === 'full-screen') {
-      return (
-        <Host class="p-loader">
-          <div class="loading-screen">
-            <div class="content">
-              <slot />
-
-              <div class="loader-wrapper">{this._loader}</div>
-            </div>
-          </div>
-        </Host>
-      );
+    componentShouldUpdate() {
+        this._checkShow();
     }
 
-    return <Host class={`p-loader flex ${this.variant === 'full-width' && 'w-100 d-block flex justify-center text-4xl'}`}>{this._loader}</Host>;
-  }
+    render() {
+        if (!this._show) {
+            return;
+        }
 
-  private _checkShow() {
-    if (this._showSubscriber) {
-      this._showSubscriber.unsubscribe();
-      this._showSubscriber = null;
+        if (this.variant === 'full-screen') {
+            return (
+                <Host class="p-loader variant-full-screen">
+                    <div class="loading-screen">
+                        <div class="content">
+                            <slot />
+
+                            <div class="loader-wrapper">{this._loader}</div>
+                        </div>
+                    </div>
+                </Host>
+            );
+        }
+
+        return (
+            <Host
+                class={`p-loader flex ${
+                    this.variant === 'full-width' &&
+                    'w-full d-block flex justify-center text-4xl'
+                }`}
+            >
+                {this._loader}
+            </Host>
+        );
     }
 
-    if (typeof this.show !== 'boolean') {
-      this._showSubscriber = this.show?.subscribe(show => (this._show = show));
-      return;
-    }
+    private _checkShow() {
+        if (this._showSubscriber) {
+            this._showSubscriber.unsubscribe();
+            this._showSubscriber = null;
+        }
 
-    this._show = this.show;
-  }
+        if (typeof this.show !== 'boolean') {
+            this._showSubscriber = this.show?.subscribe(
+                (show) => (this._show = show)
+            );
+            return;
+        }
+
+        this._show = this.show;
+    }
 }

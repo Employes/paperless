ce04f3b6d241b97e65b953478e54b7b84b6f7eeb
Jurien Hamaker <jurien@employes.nl>
2022-07-14 13:02:50 +0200

    feat(Bosons/Border Radius): Add xxlarge variant

--- packages/core/src/tailwind/border-radius.js
+++ packages/core/src/tailwind/border-radius.js
@@ -1,9 +1,10 @@
 module.exports = {
-  xlarge: '0.5625rem',
-  large: '0.375rem',
-  DEFAULT: '0.25rem',
-  medium: '0.25rem',
-  small: '0.125rem',
-  none: 0,
-  round: '100%',
+    xxlarge: '0.75rem',
+    xlarge: '0.5625rem',
+    large: '0.375rem',
+    DEFAULT: '0.25rem',
+    medium: '0.25rem',
+    small: '0.125rem',
+    none: 0,
+    round: '100%',
 };

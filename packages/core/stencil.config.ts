04ea90b9657cb3d3320899a711ec4b66f3ef7968
Jurien Hamaker <whoami@jurien.dev>
2022-07-26 17:40:17 +0200

    chore: use stenciljs `experimentalImportInjection`

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -7,53 +7,58 @@ import tailwind from 'stencil-tailwind-plugin';
 import tailwindConfig from './src/tailwind.config';
 
 export const config: Config = {
-  namespace: 'paperless',
-  globalStyle: 'src/style/paperless.scss',
-  plugins: [
-    sass(),
-    tailwind({
-      // enableDebug: true,
-      tailwindConf: tailwindConfig,
-    }),
-    inlineSvg(),
-  ],
-  devServer: {
-    address: '0.0.0.0',
-    port: 8080,
-    reloadStrategy: 'pageReload',
-  },
-  testing: {
-    browserArgs: ['--no-sandbox'],
-  },
-  outputTargets: [
-    angular({
-      componentCorePackage: '@paperless/core',
-      directivesProxyFile: '../angular/projects/paperless/src/lib/stencil/components.ts',
-      directivesArrayFile: '../angular/projects/paperless/src/lib/stencil/index.ts',
-    }),
-    react({
-      componentCorePackage: '@paperless/core',
-      proxiesFile: '../react/src/components/stencil/index.ts',
-      includeDefineCustomElements: true,
-    }),
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
+    namespace: 'paperless',
+    globalStyle: 'src/style/paperless.scss',
+    extras: {
+        experimentalImportInjection: true,
     },
-    {
-      type: 'dist-custom-elements',
+    plugins: [
+        sass(),
+        tailwind({
+            // enableDebug: true,
+            tailwindConf: tailwindConfig,
+        }),
+        inlineSvg(),
+    ],
+    devServer: {
+        address: '0.0.0.0',
+        port: 8080,
+        reloadStrategy: 'pageReload',
     },
-    {
-      type: 'docs-readme',
+    testing: {
+        browserArgs: ['--no-sandbox'],
     },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
-    {
-      type: 'www',
-      dir: 'dist',
-      copy: [{ src: 'assets' }, { src: 'tailwind.config.js' }],
-    },
-  ],
+    outputTargets: [
+        angular({
+            componentCorePackage: '@paperless/core',
+            directivesProxyFile:
+                '../angular/projects/paperless/src/lib/stencil/components.ts',
+            directivesArrayFile:
+                '../angular/projects/paperless/src/lib/stencil/index.ts',
+        }),
+        react({
+            componentCorePackage: '@paperless/core',
+            proxiesFile: '../react/src/components/stencil/index.ts',
+            includeDefineCustomElements: true,
+        }),
+        {
+            type: 'dist',
+            esmLoaderPath: '../loader',
+        },
+        {
+            type: 'dist-custom-elements',
+        },
+        {
+            type: 'docs-readme',
+        },
+        {
+            type: 'www',
+            serviceWorker: null, // disable service workers
+        },
+        {
+            type: 'www',
+            dir: 'dist',
+            copy: [{ src: 'assets' }, { src: 'tailwind.config.js' }],
+        },
+    ],
 };

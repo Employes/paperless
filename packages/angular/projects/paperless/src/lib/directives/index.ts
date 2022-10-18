e04331f86bb96b5b671184ea557bb5f8e5d3c414
Jurien Hamaker <jurien@employes.nl>
2022-10-18 16:12:39 +0200

    fix(Angular): Export table directive

--- packages/angular/projects/paperless/src/lib/directives/index.ts
+++ packages/angular/projects/paperless/src/lib/directives/index.ts
@@ -2,15 +2,18 @@ export * from './p-page-size-select.directive';
 export * from './p-pagination.directive';
 export * from './p-table-footer.directive';
 export * from './p-table-header.directive';
+export * from './p-table.directive';
 
 import { PageSizeSelectDirective } from './p-page-size-select.directive';
 import { PaginationDirective } from './p-pagination.directive';
 import { TableFooterDirective } from './p-table-footer.directive';
 import { TableHeaderDirective } from './p-table-header.directive';
+import { TableDirective } from './p-table.directive';
 
 export const CUSTOM_DIRECTIVES = [
     PaginationDirective,
     PageSizeSelectDirective,
     TableFooterDirective,
     TableHeaderDirective,
+    TableDirective,
 ];

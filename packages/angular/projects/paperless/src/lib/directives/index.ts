dbb72f99e0e796bfee6ef0097e2da2b752f89f4d
Jurien Hamaker <jurien@employes.nl>
2022-10-24 16:40:15 +0200

    fix(Angular): remove p-table-column from output

--- packages/angular/projects/paperless/src/lib/directives/index.ts
+++ packages/angular/projects/paperless/src/lib/directives/index.ts
@@ -1,6 +1,5 @@
 export * from './p-page-size-select.directive';
 export * from './p-pagination.directive';
-export * from './p-table-definition.directive';
 export * from './p-table-footer.directive';
 export * from './p-table-header.directive';
 export * from './p-table.directive';
@@ -11,11 +10,11 @@ import { TableFooterDirective } from './p-table-footer.directive';
 import { TableHeaderDirective } from './p-table-header.directive';
 import { TableDirective } from './p-table.directive';
 
+// Custom directives
 export const CUSTOM_DIRECTIVES = [
     PaginationDirective,
     PageSizeSelectDirective,
     TableFooterDirective,
     TableHeaderDirective,
     TableDirective,
-    // TableDefinition,
 ];

a1c96c62b408843c8a5f2540901e910fa8beb9f4
Jurien Hamaker <jurien@employes.nl>
2022-10-28 12:31:56 +0200

    feat(Atoms/Table Container): Enable shadow dom

--- packages/core/src/components/atoms/table-container/table-container.component.tsx
+++ packages/core/src/components/atoms/table-container/table-container.component.tsx
@@ -3,6 +3,7 @@ import { Component, h, Host } from '@stencil/core';
 @Component({
     tag: 'p-table-container',
     styleUrl: 'table-container.component.scss',
+    shadow: true,
 })
 export class TableContainer {
     render() {

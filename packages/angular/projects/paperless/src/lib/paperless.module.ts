65fa8e777790941dc1b368cad5ebf0fe2bce07e2
Jurien Hamaker <jurien@employes.nl>
2022-11-03 12:32:05 +0100

    feat(Angular): Export sub modules

--- packages/angular/projects/paperless/src/lib/paperless.module.ts
+++ packages/angular/projects/paperless/src/lib/paperless.module.ts
@@ -1,21 +1,16 @@
 import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { DIRECTIVES } from './directives';
-import { PaperlessTableModule, PaperlessToastModule } from './modules';
+import * as MODULES from './modules';
 import { PIPES } from './pipes';
 import { PaperlessStencilModule } from './stencil.module';
 
 const NGX_PIPES = [DatePipe, CurrencyPipe];
 
 @NgModule({
-    imports: [
-        CommonModule,
-        PaperlessStencilModule,
-        PaperlessTableModule,
-        PaperlessToastModule,
-    ],
+    imports: [CommonModule, PaperlessStencilModule, ...MODULES],
     declarations: [...DIRECTIVES, ...PIPES],
-    exports: [PaperlessStencilModule, ...DIRECTIVES, ...PIPES],
+    exports: [PaperlessStencilModule, ...MODULES, ...DIRECTIVES, ...PIPES],
     providers: [...NGX_PIPES, ...PIPES],
 })
 export class PaperlessModule {}

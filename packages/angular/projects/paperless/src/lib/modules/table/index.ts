3e8fb90b075c218904f7fee15e2a6e37f8c63d8e
Jurien Hamaker <jurien@employes.nl>
2022-11-16 13:14:24 +0100

    feat(Angular/Table): Add utility

--- packages/angular/projects/paperless/src/lib/modules/table/index.ts
+++ packages/angular/projects/paperless/src/lib/modules/table/index.ts
@@ -2,3 +2,4 @@ export * from './base';
 export * from './components';
 export * from './directives';
 export * from './table.module';
+export * from './utils';
---
+++ packages/angular/projects/paperless/src/lib/modules/table/util.ts
@@ -0,0 +1,33 @@
+import { TableQuickFilter } from './base';
+
+export const createFormFilters = (
+    values: { [key: string]: any },
+    quickFilters: TableQuickFilter[],
+    quickFilterKey: string
+) => {
+    const filters = [];
+    let quickFilter = null;
+
+    for (const key of Object.keys(values)) {
+        const value = values[key];
+        if (key !== quickFilterKey && !value?.length) {
+            continue;
+        }
+
+        if (key === quickFilterKey) {
+            quickFilter = quickFilters.find((f) => f.value === value);
+
+            continue;
+        }
+
+        filters.push({
+            key,
+            value,
+        });
+    }
+
+    return {
+        filters,
+        quickFilter,
+    };
+};

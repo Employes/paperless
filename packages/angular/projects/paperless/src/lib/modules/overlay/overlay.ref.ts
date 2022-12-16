1779bc3ffa32c86e7f07ca834249d3e14d1f6675
Jurien Hamaker <jurien@employes.nl>
2022-12-16 12:39:54 +0100

    feat(Angular/OverlayRef): Add `closed$` subject

--- packages/angular/projects/paperless/src/lib/modules/overlay/overlay.ref.ts
+++ packages/angular/projects/paperless/src/lib/modules/overlay/overlay.ref.ts
@@ -1,11 +1,14 @@
 import { OverlayRef as CDKOverlayRef } from '@angular/cdk/overlay';
+import { Subject } from 'rxjs';
 
 export class OverlayRef<T> {
     public instance!: T;
+    public closed$ = new Subject();
 
     constructor(private _overlay: CDKOverlayRef) {}
 
     close(): void {
         this._overlay.dispose();
+        this.closed$.next(null);
     }
 }

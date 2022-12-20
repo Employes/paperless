511cb9bcd813b0923a22831877b85e2e2a369fb9
Jurien Hamaker <jurien@employes.nl>
2022-12-20 14:20:30 +0100

    feat(Angular): Added datepicker directive

--- packages/angular/projects/paperless/src/lib/directives/index.ts
+++ packages/angular/projects/paperless/src/lib/directives/index.ts
@@ -1,7 +1,9 @@
+export * from './p-datepicker.directive';
 export * from './p-page-size-select.directive';
 export * from './p-pagination.directive';
 export * from './p-select.directive';
 
+import { DatepickerDirective } from './p-datepicker.directive';
 import { PageSizeSelectDirective } from './p-page-size-select.directive';
 import { PaginationDirective } from './p-pagination.directive';
 import { SelectDirective } from './p-select.directive';
@@ -10,4 +12,5 @@ export const DIRECTIVES = [
     PaginationDirective,
     PageSizeSelectDirective,
     SelectDirective,
+    DatepickerDirective,
 ];

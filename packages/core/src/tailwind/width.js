88b8584739afdfce597414789aab5d630db7f6ed
Jurien Hamaker <whoami@jurien.dev>
2024-03-25 12:49:09 +0100

    fix(Atoms/Layout): Make sure widths are excluding padding now

--- packages/core/src/tailwind/width.js
+++ packages/core/src/tailwind/width.js
@@ -4,15 +4,13 @@ module.exports = {
 
 		// default variant
 		'content-mobile': '100%',
-		'content-1280': '65rem',
-		'content-1440': '75rem',
-		'content-1536': '81rem',
-		'content-1680': '87rem',
-		'content-1920': '90rem',
-		'content-2560': '92.5rem',
+		'content-1280': '62rem',
+		'content-1440': '71rem',
+		'content-1536': '77rem',
+		'content-1680': '83rem',
 
 		// login variant
 		'sidebar-desktop-login': '100%',
-		'content-desktop-login': '30rem',
+		'content-desktop-login': '21rem',
 	},
 };

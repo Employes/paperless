f266f62efebf43a864f43d62730682d3431f3dad
Jurien Hamaker <whoami@jurien.dev>
2024-08-21 11:04:25 +0200

    fix(angular/date): Set default for format

--- packages/angular/projects/paperless/src/lib/pipes/date.pipe.ts
+++ packages/angular/projects/paperless/src/lib/pipes/date.pipe.ts
@@ -2,12 +2,12 @@ import { DatePipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'pdate',
+	name: 'pdate',
 })
 export class CustomDatePipe implements PipeTransform {
-    constructor(private _datePipe: DatePipe) {}
+	constructor(private _datePipe: DatePipe) {}
 
-    transform(value: any, format: 'dd MMM yyyy') {
-        return this._datePipe.transform(value, format);
-    }
+	transform(value: any, format = 'dd MMM yyyy') {
+		return this._datePipe.transform(value, format);
+	}
 }
